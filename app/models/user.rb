fde8f7448b899619399186942518c77b722179b1
Jessica Hines <jessicahines717@gmail.com>
2017-07-10 13:28:36 -0400

    added validations on models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :address, presence: true
+  validates :email, presence: true, uniqueness: true
+  has_many tools
+  has_many bookings
 end
