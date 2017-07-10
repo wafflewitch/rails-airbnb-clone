b063b7991582a2679879910961be3cf493a04e1a
Louis de La Marnierre <empsalle@gmail.com>
2017-07-10 13:40:38 -0400

    filled the tools controller

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -1,13 +1,46 @@
 class ToolsController < ApplicationController
+  before_action :set_tool, only: [ :show, :edit, :update, :destroy]
   def new
+    @tool = Tool.new
   end
 
   def edit
   end
 
   def show
+    @tool = Tool.find(params[:id])
   end
 
   def index
+    @Tools = Tool.all
+  end
+
+  def update
+    @tool.update(tool.params)
+    redirect_to tools_path
+  end
+
+  def create
+    @tool = Tool.new(tool_params)
+    if @tool.save
+      redirect_to tool_path(@tool)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @tool.destroy
+    redirect_to tools_path
+  end
+
+  private
+
+  def tool_params
+    params.require(:tool).permit(:name)
+  end
+
+  def set_tool
+    @tool = Tool.find(params[:id])
   end
 end
