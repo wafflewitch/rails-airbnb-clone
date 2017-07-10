b7294df379816f449605232e26e27961bf920dc9
Louis de La Marnierre <empsalle@gmail.com>
2017-07-10 13:18:24 -0400

    created all the controllers

---
+++ app/controllers/tools_controller.rb
@@ -0,0 +1,13 @@
+class ToolsController < ApplicationController
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def index
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+end
