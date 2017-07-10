dfc5c5a8fa0971624e35d5106ded2785d449bff9
Jessica Hines <jessicahines717@gmail.com>
2017-07-10 17:21:32 -0400

    fixed issue with mount_uploader line. just needed to restart local server.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  # mount_uploader :photo, PhotoUploader
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  mount_uploader :photo, PhotoUploader
 end
