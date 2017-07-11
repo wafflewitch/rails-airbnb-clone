e23d63a7dda81ee0b5ff54830d1fd3fcae4bf188
Sophie Meier <mesophie@gmx.ch>
2017-07-11 18:01:39 -0400

    edited form (brokern)

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -1,10 +1,12 @@
 class Tool < ApplicationRecord
+  CATEGORIES = ["power tools", "books", "transport", "hand tools", "sport", "painting"]
   belongs_to :user
   has_many :reviews, through: :booking
-  validates :category, presence: true
+  validates :category, inclusion: { in: CATEGORIES }
   # validates :description
   validates :title, presence: true
   validates :price, presence: true
   validates :available, presence: true
   validates :photo, presence: true, uniqueness: true
+
 end
