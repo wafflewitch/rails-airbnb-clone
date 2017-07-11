58eb4a9ded09015b8eb68b960a5b9252d4ebfb2b
Sophie Meier <mesophie@gmx.ch>
2017-07-11 11:01:30 -0400

    removed seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-tool = Tool.new(user_id: 1, category: "power", description: "very useful", title: "charger", price: 4, available: true, photo: "http://lorempixel.com/400/400/nature")
-tool.save!
+
