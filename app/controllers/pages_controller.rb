087ede38ba117eea31d1afdd004c029f14f7ef0a
Jessica Hines <jessicahines717@gmail.com>
2017-07-12 19:00:30 -0400

    fixed error in syntax with cl_image_tag

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
+
   def home
     @categories = []
     Tool::CATEGORIES.each do |category|
@@ -17,12 +18,9 @@ class PagesController < ApplicationController
     end
   end
 
-
-
   def profile
     @tools = Tool.where(user_id: current_user.id)
     @user = current_user
   end
-
 end
 
